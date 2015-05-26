e7e7469a9885daf65f2b36dcb7d06cfce88801a7
Daniel <905373486@qq.com>
2015-05-26 20:52:30 +0800

    Begin Express @ 150526 20:52

---
+++ helloWorld.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Daniel on 2015/5/26.
+ */
+
+var http = require('http');
+
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello world!');
+}).listen(3000);
+
+console.log('Server started on localhost:3000; press Ctrl-C to terminate......');
