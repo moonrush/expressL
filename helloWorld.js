1d4b6820e263466c0f79b76667daccf553064566
Daniel <lizhe.0625@gmail.com>
2015-05-27 11:27:48 +0800

    update static service

--- helloWorld.js
+++ helloWorld.js
@@ -2,7 +2,8 @@
  * Created by Daniel on 2015/5/26.
  */
 
-var http = require('http');
+
+/*var http = require('http');
 
 http.createServer(function(req,res){
 
@@ -12,7 +13,8 @@ http.createServer(function(req,res){
     res.end('Hello World!');
     */
 
-    //规范化url，去掉查询字符串、可选的反斜杠，并把它变成小写
+    /*
+    // 规范化url，去掉查询字符串、可选的反斜杠，并把它变成小写
     var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
     switch(path){
         case  '':
@@ -26,6 +28,41 @@ http.createServer(function(req,res){
         default:
             res.writeHead(404,{ 'Content-Type': 'text/plain' });
             res.end('Not Found');
+            break;
+    }
+
+}).listen(3000);*/
+
+// 静态资源服务
+var http = require('http'), fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode){
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data){
+        if(err){
+            res.writeHead(500, { 'Conten-Type':'text/plain' });
+            res.end('500 - Internal Error');
+        }else{
+            res.writeHead(responseCode,{ 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+}
+http.createServer(function(req,res){
+    // 规范化url，去掉查询字符串、可选的反斜杠，并把它变成小写
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch (path){
+        case '':
+            serveStaticFile(res, '/public/home.html', 'text/html');
+            break;
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo':
+            serveStaticFile(res, '/public/img/logo.png', 'image/jpeg');
+            break;
+        default:
+            serveStaticFile(res, '/public/404.html', 'text/html',404);
             break;
     }
 }).listen(3000);
